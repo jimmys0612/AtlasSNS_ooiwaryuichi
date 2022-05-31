7b32b1dd76254c6c3257f7d7fad36ef0f4a89b8a
ohiwaRyuichi <fotkano.suxen@gmail.com>
2022-05-31 16:20:47 +0900

    再度AtlasSNSのデータを追加しました

--- AtlasSNS/storage/framework/views/4dc681b163df1135397657358496622d4148625e.php
+++
@@ -1,5 +0,0 @@
-<?php $__env->startSection('content'); ?>
-<h2>機能を実装していきましょう。</h2>
-
-<?php $__env->stopSection(); ?>
-<?php echo $__env->make('layouts.login', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\Laravel\Laravel\resources\views/posts/index.blade.php ENDPATH**/ ?>
